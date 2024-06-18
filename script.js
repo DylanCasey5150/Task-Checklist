97e79c7b2d3a65626dfc05af96e6169fc875207b
Dylan Casey <dylan.casey.john@gmail.com>
2024-06-18 14:12:47 -0400

    Add Main Title, 3 boxes with titles and list components each with dummy data. Create button for today and queue. Fetch data from JSON file and display it in <li/>

---
+++ script.js
@@ -0,0 +1,34 @@
+const display = document.querySelector("#display-data-done-main");
+
+/* FETCH JSON DATA */
+
+
+const getData = () =>{
+  fetch('./user.json')
+  .then(res => res.json())
+  .then(data=>{;
+    console.log(data);
+   return data;
+  })
+}
+
+const displayData = () =>{
+  const payload = getData();
+  console.log(payload);
+ 
+  
+  let dataDisplay = payload.map((object) => {
+   const { task, due, importance, notes } = object;
+
+  return `
+  <li>
+  <div class="item item--name">{task}</div>
+  <div class="item item--date">{due}</div>
+  </li>
+  `
+  });
+
+  display.innerHTML = dataDisplay;
+}
+
+displayData();
